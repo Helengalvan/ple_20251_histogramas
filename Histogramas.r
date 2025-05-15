71c6581f396231475a4dd654e3a1b8aef7937fc5
gerbayona <gebayonaa@unal.edu.co>
2025-05-15 10:43:41 -0500

    Update Histogramas.r

--- Histogramas.r
+++ Histogramas.r
@@ -12,3 +12,29 @@ hist(temperatura,
 
 runif (1, min = 0, max = 1) # runif se usa para generar numeros aleatirios 
 runif (2, min = 0, max = 1) # genero dos nmero s aleatorios entre 0 y 1
+
+# t unif     t normal
+
+temp_uniforme <-runif(1000, min=10, max=30)
+temp_normal <- rnorm(1000, mean=20, sd=5)
+# crear un data frame para la tabla
+tabla_uniforme <-data.frame(temperatura=temp_uniforme)
+tabla_normal <-data.frame(temperatura=temp_normal)
+
+par(mfrow=c(1,2)) # dividir la ventana de graficos en 1 fila y 2 columnas
+hist(tabla_uniforme$temperatura,
+     main = "Temperaturas Uniformes",
+     xlab = "temperatura(°C)",
+     ylab = "frecuencia", 
+     col = "lightblue", 
+     border = "black",
+     breaks = 9,
+     xlim = c(10,30))
+hist(tabla_normal$temperatura,
+     main = "Temperaturas Normales",
+     xlab = "temperatura(°C)",
+     ylab = "frecuencia", 
+     col = "lightblue", 
+     border = "black",
+     breaks = 9,
+     xlim = c(10,30))
