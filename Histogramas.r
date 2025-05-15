117882301f5861b48c42dce58742bcce2ea897c5
gerbayona <gebayonaa@unal.edu.co>
2025-05-15 10:16:23 -0500

    Update Histogramas.r

--- Histogramas.r
+++ Histogramas.r
@@ -1,10 +1,14 @@
 # Histogramas
-temperatura <- c(29.8, 32.4, 35.3, 27.2, 29.3, 30.4)
+temperatura <- runif (25, min= 27, max= 36) # runif genera 1000 numeros aleatorios entre 27 y 36
+
 hist(temperatura,
-     main = "Hisrograma de temperatura",
+     main = "Temperaturas en la ciudad de la Paz",
      xlab = "temperatura(°C)",
      ylab = "frecuencia", 
      col = "lightblue", 
      border = "black",
-     breakc = 9,
+    breaks = 9,
      xlim = c(27,36))
+
+runif (1, min = 0, max = 1) # runif se usa para generar numeros aleatirios 
+runif (2, min = 0, max = 1) # genero dos nmero s aleatorios entre 0 y 1
